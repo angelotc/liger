c395084251dd216a6f09dfcf960f017a0e05c96b
angelotc <angelocortez102@gmail.com>
2026-06-03 21:05:09 -0700

    add scripts

--- scripts/ablation.sh
+++ scripts/ablation.sh
@@ -16,7 +16,7 @@ python run.py \
     method.embedding_head_dict.embed_target="ground_truth+item_id" \
     method.embedding_head_dict.embed_proj_type="linear" \
     method.embedding_head_dict.use_new_init=True \
-    time_budget_seconds=600 \
+    time_budget_seconds=3000 \
     experiment_id="ablation_dense_id"
 
 
@@ -30,7 +30,7 @@ python run.py \
     method=setting \
     test_method=liger \
     method.sid_loss_weight=0 \
-    time_budget_seconds=600 \
+    time_budget_seconds=3000 \
     experiment_id="ablation_dense_sid"
 
 
@@ -42,7 +42,7 @@ python run.py \
     device_id=0 \
     method=setting \
     test_method=liger \
-    time_budget_seconds=600 \
+    time_budget_seconds=3000 \
     experiment_id="ablation_liger" 
 
 
@@ -56,7 +56,7 @@ python run.py \
     test_method=liger \
     method.flag_use_output_embedding=False \
     method.embedding_loss_weight=0 \
-    time_budget_seconds=600 \
+    time_budget_seconds=3000 \
     experiment_id="ablation_tiger_text"
 
 
@@ -68,5 +68,5 @@ python run.py \
     device_id=0 \
     method=base \
     test_method=tiger \
-    time_budget_seconds=600 \
+    time_budget_seconds=3000 \
     experiment_id="ablation_tiger"
